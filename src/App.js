f6362f33eeee2e433345a557c457ffb791855066
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-06-16 12:50:39 -0300

    adicionado estado no filtro

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.css'
 import Produtos from './components/produtos/index';
-import Filtro from './components/filtro/filtro'
-import styled from "styled-components"
+import Filtro from './components/filtro/filtro';
+import styled from "styled-components";
+
+const Container = styled.div`
+display: grid;
+grid-template-columns: 25% 50% 25%;
+`
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <Container>
         <Filtro/>
         <Produtos/>
-      </div>
+      </Container>
     );
   }
 }
