be200e035998bbe9a13303c83f847a142bbf88cc
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-06-16 17:49:57 -0300

    filtro home e carrinho unidos adicionado

--- src/App.js
+++ src/App.js
@@ -1,38 +1,107 @@
 
 import React, {Component} from 'react';
 import Carrinho from './components/carrinho'
-import Produtos from './components/produtos/index';
 import './App.css'
 import VitrineProdutos from './components/home/vitrine/index';
 import Filtro from './components/filtro/filtro';
-import './App.css'
-import { MainContainer } from './global-style'
 import { CartProduct } from './components/carrinho/styles';
 import { TrashIcon } from './assets/icons/icons';
 
-
 class App extends Component {
   state = {
-    cart: [
+    filtros : {
+      valorMinimo: 25,
+      valorMaximo: 1000,
+      buscaNome: 'Produto'
+    },
+    produtos: [
       {
-        id: '1',
-        quantity: 1,
-        name: "Blusa",
-        value: 300
+        id: Math.random(),
+        imagem: 'https://static.zattini.com.br/produtos/bota-coturno-feminina-vicerinne-tratorada-salto-alto/06/GYK-0049-006/GYK-0049-006_zoom1.jpg?ts=1632126026',
+        nome: 'Produto Bota',
+        value: 200
       },
       {
-        id: '2',
-        quantity: 1,
-        name: "calça",
-        value: 300
+        id: Math.random(),
+        imagem: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR3mvNErXhxOBT_t_MH_7Sai7MlzK2qVJ5erUeoe6f_wBHJ06XTSk__mtpwHH3IKc3fAtZUGwnqvAmz&usqp=CAc',
+        nome: 'Produto Luva',
+        value: 100
       },
       {
-        id: '3',
-        quantity: 1,
-        name: "tenis",
-        value: 344.50
+        id: Math.random(),
+        imagem: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR3mvNErXhxOBT_t_MH_7Sai7MlzK2qVJ5erUeoe6f_wBHJ06XTSk__mtpwHH3IKc3fAtZUGwnqvAmz&usqp=CAc',
+        nome: 'Produto Calça',
+        value: 150
+      },
+      {
+        id: Math.random(),
+        imagem: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR3mvNErXhxOBT_t_MH_7Sai7MlzK2qVJ5erUeoe6f_wBHJ06XTSk__mtpwHH3IKc3fAtZUGwnqvAmz&usqp=CAc',
+        nome: 'Produto Meia',
+        value: 25
       }
-    ]
+    ],
+    cart: []
+  }
+
+  // Lógicas para Filtrar Produtos
+
+  produtosFiltrados = () => {
+    const filtros = this.state.filtros;
+
+    const produtosFiltrados = this.state.produtos.filter((produto) => {
+      if(produto.value >= filtros.valorMinimo && produto.value <= filtros.valorMaximo && produto.nome.includes(filtros.buscaNome)) {
+        return produto;
+      }
+    })
+
+    return produtosFiltrados;
+  }
+
+  // Lógicas para Atualizar Filtros
+
+  onChangeValorMinimo = (novoValorMinimo) => {
+    this.setState({
+      filtros: {
+        ...this.state.filtros,
+        valorMinimo: novoValorMinimo
+      }
+    })
+  }
+
+  onChangeValorMaximo = (novoValorMaximo) => {
+    this.setState({
+      filtros: {
+        ...this.state.filtros,
+        valorMaximo: novoValorMaximo
+      }
+    })
+  }
+
+  onChangeBuscaNome = (novoBuscaNome) => {
+    this.setState({
+      filtros: {
+        ...this.state.filtros,
+        buscaNome: novoBuscaNome
+      }
+    })
+  }
+
+  // Logicas para Carrinho de Compras
+
+  addProdutToCart = (id) => {
+    const produtos = this.state.produtos;
+
+    const produtoFiltradoParaAdicionarAoCarrinho = produtos.filter((produto) => {
+      if(produto.id === id) {
+        return produto
+      }
+    });
+
+    this.setState({
+      cart: [
+        ...this.state.cart, {...produtoFiltradoParaAdicionarAoCarrinho[0], quantity: 1, id: Math.random()}
+      ]
+    })
   }
 
   removeProduct = (id) => {
@@ -61,11 +130,11 @@ class App extends Component {
   renderCartList = () => {
     const { cart } = this.state;
 
-    const newCartList = cart.map(({ id, name, value, quantity }) => {
+    const newCartList = cart.map(({ id, nome, value, quantity }) => {
 
       return <CartProduct key={id} >
         <span>{quantity}x</span>
-        <h4>{name}</h4>
+        <h4>{nome}</h4>
         <span>R$ {value.toFixed(2).replace(".", ",")}</span>
         <button onClick={() => this.removeProduct(id)}><TrashIcon /></button>
       </CartProduct>
@@ -73,14 +142,27 @@ class App extends Component {
 
     return newCartList;
   }
+
+  // Renderização do Template Principal
+
   render() {
     return (
-       <div>
-        <Filtro/>
-        <VitrineProdutos></VitrineProdutos>
-        <Carrinho cart={this.renderCartList()} totalValue={this.getTotalValue()}/>
+       <div className="container">
+        <Filtro
+          filtros={this.state.filtros}
+          enviarNovoValorMinimo={this.onChangeValorMinimo}
+          enviarNovoValorMaximo={this.onChangeValorMaximo}
+          enviarNovoBuscaNome={this.onChangeBuscaNome}
+        />
+        <VitrineProdutos
+          produtos={this.produtosFiltrados()}
+          enviarProdutoSelecionado={this.addProdutToCart}
+        />
+        <Carrinho
+          cart={this.renderCartList()}
+          totalValue={this.getTotalValue()}
+        />
       </div>
-
     );
   }
 }
--- src/components/home/vitrine/index.js
+++ src/components/home/vitrine/index.js
@@ -4,34 +4,30 @@ import Produto from '../criar-produto/index';
 
 // Aqui é onde os produtos são criados e expostos a venda
 
-function VitrineProdutos() {
+class VitrineProdutos extends React.Component {
 
-    return (
-        <div>
-        <div className="vitrine">
-            <Produto
-                imagem = "https://static.zattini.com.br/produtos/bota-coturno-feminina-vicerinne-tratorada-salto-alto/06/GYK-0049-006/GYK-0049-006_zoom1.jpg?ts=1632126026"
-                nome="bota"
-                valor="200,00"
-            ></Produto>
-            <Produto
-                imagem = "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR3mvNErXhxOBT_t_MH_7Sai7MlzK2qVJ5erUeoe6f_wBHJ06XTSk__mtpwHH3IKc3fAtZUGwnqvAmz&usqp=CAc"
-                nome="luva"
-                valor="100,00"
-            ></Produto>
-            <Produto
-                imagem = "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR3mvNErXhxOBT_t_MH_7Sai7MlzK2qVJ5erUeoe6f_wBHJ06XTSk__mtpwHH3IKc3fAtZUGwnqvAmz&usqp=CAc"
-                nome="eqweqw"
-                valor="130,00"
-            ></Produto>
-            <Produto
-                imagem = "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR3mvNErXhxOBT_t_MH_7Sai7MlzK2qVJ5erUeoe6f_wBHJ06XTSk__mtpwHH3IKc3fAtZUGwnqvAmz&usqp=CAc"
-                nome="luvwqeqwea"
-                valor="160,00"
-            ></Produto>
-        </div>
-        </div>
-    )
+    enviarProdutoSelecionado = (id) => {
+        this.props.enviarProdutoSelecionado(id);
+    }
+
+    render() {
+        return(
+            <div className="vitrine">
+                {this.props.produtos.map((produto) => {
+                    return (
+                        <Produto
+                            id={produto.id}
+                            key={produto.id}
+                            imagem={produto.imagem}
+                            nome={produto.nome}
+                            value={produto.value}
+                            produtoSelecionado={this.enviarProdutoSelecionado}
+                        />
+                    )
+                })}
+            </div>
+        )
+    }
 }
   
 export default VitrineProdutos;
