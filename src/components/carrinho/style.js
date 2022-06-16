fe7f84bf3cf2d25998b72b6829c8a2fc4d78ab8b
marciadpmell <mdepmell@gmail.com>
2022-06-15 22:37:49 -0300

    marcia carrinho

---
+++ src/components/carrinho/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const CarrinhoContainer = styled.div `
+
+`
+
+export const ProductList = styled.div `
+
+`
+
+export const Product = styled.div `
+
+`
