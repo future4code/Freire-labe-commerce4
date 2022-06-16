bc1ec3090dc74a5bdf1566966356062d1c4a41d8
marciadpmell <mdepmell@gmail.com>
2022-06-16 14:00:35 -0300

    marcia carrinho

--- src/App.js
+++ src/App.js
@@ -1,20 +1,87 @@
-import React from 'react';
-import './App.css'
-import Produtos from './components/produtos/index';
-import Filtro from './components/filtro/filtro'
-import Carrinho from './components/carrinho/carrinho';
-import styled from "styled-components"
+import React, { Component } from 'react'
 
-class App extends React.Component {
+// components
+import Carrinho from './components/cart'
+
+// styles
+import { MainContainer } from './global-style'
+import { CartProduct } from './components/cart/styles';
+
+// icons
+import { TrashIcon } from './assets/icons/icons';
+
+class App extends Component {
+  state = {
+    cart: [
+      {
+        id: '1',
+        quantity: 1,
+        name: "Blusa",
+        value: 300
+      },
+      {
+        id: '2',
+        quantity: 1,
+        name: "calça",
+        value: 300
+      },
+      {
+        id: '3',
+        quantity: 1,
+        name: "tenis",
+        value: 344.50
+      }
+    ]
+  }
+
+  removeProduct = (id) => {
+    const { cart } = this.state
+
+    const newCart = cart.filter((product) => {
+      return product.id !== id
+    })
+
+    this.setState({
+      cart: newCart
+    })
+  }
+
+  getTotalValue = () => {
+    const { cart } = this.state
+    let totalValue = 0
+
+    cart.map((product) => {
+        return totalValue = totalValue + ( product.value * product.quantity )
+    })
+
+    return totalValue.toFixed(2).replace(".", ",")
+  }
+
+  renderCartList = () => {
+    const { cart } = this.state;
+
+    const newCartList = cart.map(({id, name, value, quantity}) => {
+
+      return <CartProduct key={id} >
+          <span>{quantity}x</span>
+          <h4>{name}</h4>
+          <span>R$ {value.toFixed(2).replace(".", ",")}</span>
+          <button onClick={() => this.removeProduct(id)}><TrashIcon/></button>
+      </CartProduct>
+    })
+
+    return newCartList;
+  }
+
   render() {
+
     return (
-      <div>
-        <Filtro/>
-        <Produtos/>
-        <Carrinho/>
-      </div>
-    );
+      <MainContainer>
+
+        <Carrinho cart={this.renderCartList()} totalValue={this.getTotalValue()}/>
+      </MainContainer>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/cart/styles.js
@@ -0,0 +1,91 @@
+import styled from 'styled-components';
+
+export const CartContainer = styled.section`
+    width: ${({ open }) => (open ? '20%' : '12%')};
+    min-height: ${({ open }) => (open ? '70%' : '20px')};
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    border: 1px solid black;
+    border-radius: ${({ open }) => (open ? '0' : '0 0 0 40px')};
+    padding: 10px;
+    position: relative;
+    transition: all 0.3s ease;
+    scroll-behavior: smooth;
+
+    h3 {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        padding: ${({ open }) => (open ? '0 0 10px' : '0')};
+        border-bottom: ${({ open }) => (open ? '1px solid #0e0e0e' : 'none')};
+
+        svg {
+            max-height: 50px;
+            max-width: 40px;
+
+            :last-child {
+                height: 25px;
+                visibility: ${({ open }) => (open ? 'visible' : 'hidden')};
+                position: absolute;
+                right: 20px;
+                transition: all 0.1s ease;
+            }
+        }
+    }
+
+    section {
+        width: 100%;
+        height: auto;
+        max-height: 530px;
+        overflow: auto;
+
+        h2 {
+            text-align: center;
+            margin: 20px 0 0;
+        }
+        
+        ::-webkit-scrollbar {
+            display: none;
+        }
+    }
+
+    p {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        position: absolute;
+        bottom: 10px;
+        font-size: 20px;
+
+        b {
+            font-size: 20px;
+            margin: 0 20px 0 0;
+        }
+    }
+`
+
+export const CartProduct = styled.div `
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 10px;
+
+    button {
+        background-color: transparent;
+        border: none;
+    }
+    svg {
+        cursor: pointer;
+        transition: all 0.3s ease;
+
+        :hover {
+            fill: red;
+        }
+    }
+`
